1030d030f68cec672db7dd033cd238431aa34f6b
JVM狗 <niezhihong1@163.com>
2015-08-17 16:10:42 +0800

    delete trunk

--- maven.demo/demo-common/src/main/java/com/lingcaibao/util/MarketMemcacheKeyConstants.java
+++
@@ -1,7 +0,0 @@
-package com.lingcaibao.util;
-
-public class MarketMemcacheKeyConstants
-{
-	static public int		ACCESS_TOKEN_TIME_OUT	= 60 * 60 * 1;		// 令牌失效时间(秒)
-	static public String	ACCESS_TOKEN_PREFIX		= "access_token_";	// 令牌前缀
-}
